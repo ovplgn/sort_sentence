c5f9bc578800490e8cc112b93456bc253e934022
ovplgn <54017139+ovplgn@users.noreply.github.com>
2021-12-12 03:23:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ if __name__=='__main__':
 def sort_sentence(sentence):
     output = sentence.lower().split()
     output.sort(key = len)
+    output[0] = output[0].capitalize()
     return print(*output)
 sort_sentence(sentence)
